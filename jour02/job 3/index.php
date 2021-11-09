e28f9309a7ea47564902b5a213bf865994bd4f3f
rhali <rhalida.adjeroud@laplatforme.io>
2021-11-09 06:10:24 +0100

    afficher nombre de 0 à 100

---
+++ jour02/job 3/index.php
@@ -0,0 +1,23 @@
+<?php
+for($i = 0 ; $i <= 100 ; $i ++){
+    if($i >=0 && $i <=20){
+        echo "<i>$i<i/><br/>";
+   }
+else if($i == 42){
+    echo"La Plateforme "."<br/>";
+}
+else if($i >=25 && $i <=50){
+    echo "<u>$i</u><br/>";
+}
+else { 
+    echo $i."<br/>";
+} 
+}
+
+
+
+
+
+
+
+?>
