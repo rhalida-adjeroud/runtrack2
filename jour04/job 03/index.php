103a0970ffad87a40cdf8a9b89b32dd47b03c0d0
rhali <rhalida.adjeroud@laplatforme.io>
2021-11-09 06:14:16 +0100

    afficher valeur tableau

---
+++ jour04/job 03/index.php
@@ -0,0 +1,25 @@
+<?php
+var_dump($_POST);
+foreach ($_POST as $key=> $value){
+    echo "$value" ." <br/>";
+} 
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+<body>
+
+<form action="index.php" method="POST">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname" value=""><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+
+</body>
+</html>
+
