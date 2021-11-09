7097d870deda6cba36c9c15fc59cdef2d5c7ffb5
rhali <rhalida.adjeroud@laplatforme.io>
2021-11-09 20:34:08 +0100

    ajustement cookies

--- jour06/job 02/index.php
+++ jour06/job 02/index.php
@@ -1,17 +1,18 @@
 <?PHP
-session_start();
-if((!isset($_cookies['nbvisites']))){
-    $_cookies['nbvisites']=0;
-}
-else{
-    $_cookies['nbvisites']++;
-}
-if((isset($_POST['reset']))){
-    session_destroy();
-    $_session_destroy['nbvisites']=0;
-}
-echo $_cookies['nbvisites'] . "bye, merci pour le tchek";
+var_dump('nbvisites');
 
+$nbvisites=0;
+if((!isset($_COOKIE['nbvisites']))){
+    $nbvisites+1;
+    setcookie('nbvisites', $nbvisites);
+}
+echo "bye, merci pour le tchek".  $_COOKIE['nbvisites'];
+if((isset($_POST['reset']))){
+    unset($_COOKIE['nbvisites']); 
+}else{
+    $_COOKIE['nbvisites']++;
+   
+}
 ?>
 <!DOCTYPE html>
 <html>
