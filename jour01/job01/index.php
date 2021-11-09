4f7c8e5e58dd6e58deecad19115f66a00b6fa07c
rhali <rhalida.adjeroud@laplatforme.io>
2021-11-09 06:07:14 +0100

    Valeur la plateforme

---
+++ jour01/job01/index.php
@@ -0,0 +1,20 @@
+<?php
+echo "hello Laplateforme";
+?>
+<br>
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 ="!";
+echo "$str "."$str2 "."$str3 ";
+$val = 6 ;
+echo "$val ";
+echo "$val"+ 4 ;
+?>
+<br>
+
+<?php
+$myBool = "var";
+$var1 = true;
+echo "$var1 "."myBool ";
+?>
