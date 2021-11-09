402a965750ec4768f5f765d9fbc0dc42b43ad5c9
rhali <rhalida.adjeroud@laplatforme.io>
2021-11-09 06:22:16 +0100

    pas fini

---
+++ jour05/job 06/index.php
@@ -0,0 +1,19 @@
+<?php
+function leetspeak($str){
+    $str1('A','a','B','b','E','e','G','g','L','l','S','s','T','t'){
+        if($i=0;isset($str[$i];$i++)){
+            
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+ù
+?>
