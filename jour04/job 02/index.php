df7b1245cc94c4205a49d9a203e485bfb2f3166c
rhali <rhalida.adjeroud@laplatforme.io>
2021-11-09 06:13:46 +0100

    tableau POST

---
+++ jour04/job 02/index.php
@@ -0,0 +1,41 @@
+<?php
+var_dump($_GET);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+<body>
+
+<form action="index.php" method="get">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname" value=""><br><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
+<table>
+<thead>
+              <tr>
+                  <th>argument</th>
+                  <th>valeur</th>
+                  
+              </tr>
+          </thead>
+          <tbody>
+              <?php
+          foreach($_GET as $k=> $value){
+          echo "<tbody>
+              <tr>
+                <td>$k</td>
+                <td>$value</td>
+             </tr>";
+            }
+            ?>
+          </tbody>
+</table>
+
+
+</body>
+</html>
