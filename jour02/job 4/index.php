483c5c0d20d2033d3600d8579fa14ee69f1d425d
rhali <rhalida.adjeroud@laplatforme.io>
2021-11-09 06:10:46 +0100

    listez lez nombre

---
+++ jour02/job 4/index.php
@@ -0,0 +1,24 @@
+<?php
+for($i =1 ; $i <= 100; $i ++){
+
+if($i% 3==0 && $i% 5==0){
+    echo "fizzBuzz";
+  
+}
+elseif($i % 3==0){
+    echo "fizz";
+}
+elseif($i % 5==0){
+    echo "buzz";
+}
+ echo $i."<br/>";
+}
+
+
+
+
+
+
+
+
+?>
