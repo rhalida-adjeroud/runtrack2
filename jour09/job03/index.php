0f7f4778c94142dfc9f4ba6c4ecd5e03e87765a7
rhali <rhalida.adjeroud@laplatforme.io>
2021-11-12 15:24:15 +0100

    naissance sexe femme

---
+++ jour09/job03/index.php
@@ -0,0 +1,40 @@
+<?php
+    $bdd = mysqli_connect('localhost','root','','jour08');
+    mysqli_set_charset($bdd , 'utf8');
+    $requete = mysqli_query($bdd,"SELECT prenom, nom, naissance FROM etudiants WHERE  sexe = 'Femme' ");
+    $etudiants= mysqli_fetch_all($requete, MYSQLI_ASSOC);
+
+?>
+<!doctype html>
+<html lang= "en">
+    <head>
+        <title>tableau sql php</title>
+    </head>
+    <body>
+        <table cellspacing="3", border=2>
+            <thead>
+            
+       
+                <th>nom</th>
+                <th>prenom</th>
+                <th>naissance</th>      
+            </thead>
+        
+    </body>
+    <tbody>
+        <?php
+        foreach($etudiants as $etudiant){
+            echo'<tr>';
+                foreach($etudiant as $infos => $value){
+                echo '<td>' .$value. '</td>';
+                }
+            echo '</tr>'; 
+        }
+        ?>
+    </table>
+    </tbody>
+
+
+
+
+</html>
